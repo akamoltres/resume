ba333103b687ab56399ac7265b8c3fe339a6166e
Abhi Krishnan <akamoltres@gmail.com>
2023-10-15 18:35:53 -0700

    fix resume.sh to handle clean commits

--- revision.sh
+++ revision.sh
@@ -3,4 +3,6 @@ echo -n $(git rev-parse --abbrev-ref HEAD) > branch.tmp
 echo -n "$(git rev-parse HEAD)"  > rawcommit.tmp
 if [ -n "$(git status --porcelain)" ] ; then
 	echo -n "$(git rev-parse --short HEAD)+" > prettycommit.tmp;
+else
+	echo -n "$(git rev-parse --short HEAD)" > prettycommit.tmp;
 fi
